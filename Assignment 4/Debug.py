103862355bc006267dda11a9ff3cb4e7ad03dc85
CamAchorn <112398447+CamAchorn@users.noreply.github.com>
2022-10-23 19:24:21 -0400

    Update Debug.py

--- Assignment 4/Debug.py
+++ Assignment 4/Debug.py
@@ -1,15 +1,39 @@
-#this file is incomplete, do not complete ahead of time
-def main():
-    var1= 10
-    var2= 20
-    #What do we put in here to make it work?
-    addition()
+def printHello():
+    print("Hello")
 
-
+def printName(x):
+    print(x)
 
 def addition(x, y):
-    #add x and y together and return them
-    return
+    z=0
+    z = x + y
+    return z
+
+
+def smaller(i, j):
+    if (i >j):
+        return(i)
+    elif (j >i):
+        return(j)
+    elif (i==j):
+        return 0
+
+
+def main():
+    printHello()
+
+    printName("Cam")
+
+
+    var1 = 10
+    var2 = 20
+    
+    print(addition(var1, var2))
+
+    num1 = int(input("Enter number 1"))
+    num2 = int(input("Enter number 2"))
+    
+    print(smaller(num1, num2))
 
 
 main()
